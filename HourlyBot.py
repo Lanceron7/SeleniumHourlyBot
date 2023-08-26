0384ff7780fd0585164fab0ca1d8dd6db088ce9c
Lanceron7 <124671449+Lanceron7@users.noreply.github.com>
2023-08-26 16:20:57 -0300

    Update HourlyBot.py

--- HourlyBot.py
+++ HourlyBot.py
@@ -15,7 +15,7 @@ driver = webdriver.Chrome(executable_path='H:\Descargas\chromedriver.exe',) ## R
 
 
 
-images =  ["1", "2", "3"] ## Replace 1, 2, 3, etc with links to the images separated by commas. 
+images =  ["1", "2", "3"] ## Replace 1, 2, 3, etc with links to the images with t.co links separated by commas. 
 ## open twitter
 driver.get('https://twitter.com/i/flow/login')
 
@@ -25,12 +25,12 @@ textbox = driver.find_element(By.XPATH, '//input[@autocorrect="on"]')
 
 textbox.send_keys("USERNAME")  ## Replace USERNAME with your account's username
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Siguiente']"))).click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Siguiente']"))).click() ## If your twitter language is not spanish, replace Siguiente with the equivalent in your language. It's the button above "Forgot my password"
 time.sleep(1)
 
 password = driver.find_element(By.XPATH, '//input[@autocomplete="current-password"]')
-password.send_keys("PASSWORD") ## Replace PASSWORD with your account's password
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Iniciar sesión']"))).click()
+password.send_keys("PASSWORD") ## Replace PASSWORD with your account's password 
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//span[text()='Iniciar sesión']"))).click() ## If your twitter language is not spanish, replace Iniciar sesión with the equivalent in your language. It's the log in button.
 WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[@contenteditable="true"]')))
 ## Post tweets
 while True:
